eea52cb54a0ee0e3604149684bd5bb95a2013d3d
MatheusMunizera <matheus.munizera@gmail.com>
2021-08-14 21:51:25 -0300

    Blank Project

---
+++ JavaApp/src/javaapp/JavaApp.java
@@ -0,0 +1,18 @@
+
+
+package javaapp;
+
+/**
+ *
+ * @author munizera
+ */
+public class JavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
