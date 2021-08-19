0e5128c4218c218f3f95aa2f7bea3eb9c2f2d9eb
MatheusMunizera <matheus.munizera@gmail.com>
2021-08-19 17:11:30 -0300

    aula2

--- palindromoExercicio3/src/palindromoexercicio3/PalindromoExercicio3.java
+++ palindromoExercicio3/src/palindromoexercicio3/PalindromoExercicio3.java
@@ -2,8 +2,10 @@
 /*
 Faça um programa recursivo que verifique se uma palavra é palindromo.
 – Exemplo: ovo, ana, abba, 010010
-*/
+ */
 package palindromoexercicio3;
+
+import java.util.*;
 
 /**
  *
@@ -16,30 +18,39 @@ public class PalindromoExercicio3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        char vetor[] = new char[]{'o', 'v', 'o'};
-        
-    
-    
-    
-        verificarPalindromo(vetor, 0);
-    }
-    
-    public static void verificarPalindromo(char[] p, int n){
-         int x = p.length;
-            if (x < n ){
-                System.out.println("Fim");
-            }else{
-                verificarPalindromo()
-            }
-     }  
-       
-        
-        
-           
-        
-     
+
+        StringBuilder palavra = new StringBuilder("ana08976");
+
+        System.out.println(verificarPalindromo(palavra));
+       //verificarPalindromo(palavra);
+
     }
 
+    public static String verificarPalindromo(StringBuilder p) {
+
+        char[] arrayed = p.toString().toCharArray();
+
+        for (int i = 0; i < p.length(); i++) {
+            for (int j = p.length() - 1; j > 0; j--) {
+                if (arrayed[i] == arrayed[j]) {
+
+                    p = p.deleteCharAt(i);
+                    p = p.deleteCharAt(j-1);
+
+                    verificarPalindromo(p);
+
+                    return "É palindromo";
+                 //   System.out.println("palin");
+                } else {
+                   return "Não é palindromo";
+                 //   System.out.println("not palin");
+                }
+
+            }
+        }
+   return "Não foi possivel verificar";
+    }
+    
 }
+        
+    
